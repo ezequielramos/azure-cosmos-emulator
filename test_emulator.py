ba4fc1ac65698c773df3d0b524eb79afb425c876
Ezequiel Ramos <Ezequiel Ramos>
2020-08-05 12:13:50 -0300

    added tests

--- test_emulator.py
+++ test_emulator.py
@@ -1,4 +1,6 @@
 import base64
+import unittest
+
 import azure.cosmos.cosmos_client as cosmos_client
 
 DATABASE = "database_name"
@@ -6,29 +8,46 @@ COLLECTION = "collection_name"
 URL_CONNECTION = "http://localhost:8081/"
 MASTER_KEY = base64.b64encode(b"dummy_password")
 
-collection_link = f"dbs/{DATABASE}/colls/{COLLECTION}"
 
-client = cosmos_client.CosmosClient(
-    url_connection=URL_CONNECTION, auth={"masterKey": MASTER_KEY}
-)
+class TestEmulator(unittest.TestCase):
+    def setUp(self):
+        self.collection_link = f"dbs/{DATABASE}/colls/{COLLECTION}"
 
-document = {"column1": "value1", "column2": "value2"}
+    def test_basic_operations(self):
 
-client.CreateItem(database_or_Container_link=collection_link, document=document)
-client.CreateItem(database_or_Container_link=collection_link, document=document)
-client.CreateItem(database_or_Container_link=collection_link, document=document)
-client.CreateItem(database_or_Container_link=collection_link, document=document)
-client.CreateItem(database_or_Container_link=collection_link, document=document)
-client.CreateItem(database_or_Container_link=collection_link, document=document)
+        client = cosmos_client.CosmosClient(
+            url_connection=URL_CONNECTION, auth={"masterKey": MASTER_KEY}
+        )
 
-query = {"query": "SELECT * FROM collection"}
+        document = {"column1": "value1", "column2": "value2"}
 
-result = client.QueryItems(
-    collection_link, query, options={"enableCrossPartitionQuery": True}
-)
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
+        client.CreateItem(
+            database_or_Container_link=self.collection_link, document=document
+        )
 
-result = list(result)
+        query = {"query": "SELECT * FROM collection"}
 
-for item in result:
-    options = {"partitionKey": item["column1"]}
-    client.DeleteItem(f"{collection_link}/docs/{item['id']}", options)
+        result = client.QueryItems(
+            self.collection_link, query, options={"enableCrossPartitionQuery": True}
+        )
+
+        result = list(result)
+
+        for item in result:
+            options = {"partitionKey": item["column1"]}
+            client.DeleteItem(f"{self.collection_link}/docs/{item['id']}", options)
